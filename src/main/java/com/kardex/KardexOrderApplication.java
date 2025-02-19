3273cd0f951b38245347a3484de908b56fa49b7a
Anjhell Reyes <anjhellreyes@gmail.com>
2025-02-19 14:10:28 -0500

    Primer commit

---
+++ src/main/java/com/kardex/KardexOrderApplication.java
@@ -0,0 +1,13 @@
+package com.kardex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KardexOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KardexOrderApplication.class, args);
+	}
+
+}
